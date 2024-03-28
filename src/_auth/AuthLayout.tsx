6865fbbe7b80540716327eb729b42d32dd686ea7
piyush960 <piyushdahake096@gmail.com>
2024-03-29 04:38:20 +0530

    image positioning

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,8 +14,8 @@ const AuthLayout = () => {
             <Outlet />
           </section>
 
-          <img src="/assets/images/side-img.svg" alt="logo" 
-          className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
+          <img src="/assets/images/side-img.png" alt="logo" 
+          className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat object-bottom'
           />
 
         </>
